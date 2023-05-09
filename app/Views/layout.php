f4192b03b93fd54227c13341ebe808bbc10035d4
abiisaleh <abi.saleh.25@gmail.com>
2023-05-09 10:33:43 +0900

    fix admin login

--- app/Views/layout.php
+++ app/Views/layout.php
@@ -177,7 +177,7 @@
                                     <a href="#" data-bs-toggle="dropdown" aria-expanded="false">
                                         <div class="user-menu d-flex">
                                             <div class="user-name text-end me-3">
-                                                <h6 class="mb-0 text-gray-600"><?= ucfirst($user['nama']) ?></h6>
+                                                <h6 class="mb-0 text-gray-600"><?= (in_groups('admin')) ? ucfirst(user()->username) : ucfirst($user['nama']) ?></h6>
                                                 <p class="mb-0 text-sm text-gray-600"><?= (in_groups('admin')) ? 'admin' : 'masyarakat' ?></p>
                                             </div>
                                             <div class="user-img d-flex align-items-center">
