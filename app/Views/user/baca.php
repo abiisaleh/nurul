c1f6b520c17319ce729017cfa8af3cbfa12fa571
abiisaleh <abi.saleh.25@gmail.com>
2023-06-13 17:52:24 +0900

    disable screeshot and print

--- app/Views/user/baca.php
+++ app/Views/user/baca.php
@@ -17,16 +17,16 @@
   <meta name="apple-mobile-web-app-status-bar-style" content="black">
   <meta name="apple-mobile-web-app-title" content="">
 
-  <base href="<?= base_url('bootstrap-pdf-viewer/') ?>">
+  <base href="<?= base_url() ?>">
 
   <!-- Twitter Bootstrap -->
-  <link rel="stylesheet" href="lib/twitter-bootstrap/css/bootstrap.css">
+  <link rel="stylesheet" href="bootstrap-pdf-viewer/lib/twitter-bootstrap/css/bootstrap.css">
 
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="lib/font-awesome/css/font-awesome.css">
+  <link rel="stylesheet" href="bootstrap-pdf-viewer/lib/font-awesome/css/font-awesome.css">
 
   <!-- Bootstrap PDF Viewer -->
-  <link rel="stylesheet" href="css/bootstrap-pdf-viewer.css">
+  <link rel="stylesheet" href="bootstrap-pdf-viewer/css/bootstrap-pdf-viewer.css">
 </head>
 
 <body>
@@ -35,22 +35,30 @@
   <div id="viewer" class="pdf-viewer" data-url="<?= base_url('uploads') ?>/<?= $id ?>-buku.pdf"></div>
 
   <!-- jQuery -->
-  <script src="lib/jquery-1.9.1.js"></script>
+  <script src="bootstrap-pdf-viewer/lib/jquery-1.9.1.js"></script>
 
   <!-- Twitter Bootstrap -->
-  <script src="lib/twitter-bootstrap/js/bootstrap.js"></script>
+  <script src="bootstrap-pdf-viewer/lib/twitter-bootstrap/js/bootstrap.js"></script>
 
   <!-- Bootstrap PDF Viewer -->
-  <script src="lib/pdf.js"></script>
-  <script src="js/bootstrap-pdf-viewer.js"></script>
+  <script src="bootstrap-pdf-viewer/lib/pdf.js"></script>
+  <script src="bootstrap-pdf-viewer/js/bootstrap-pdf-viewer.js"></script>
 
   <script>
+    var noPrint = true;
+    var noCopy = true;
+    var noScreenshot = true;
+    var autoBlur = false;
+
     var viewer;
 
     $(function() {
       viewer = new PDFViewer($('#viewer'));
     });
   </script>
+
+  <!-- NoPrint -->
+  <script src="assets/extensions/noprint/noprint.js"></script>
 </body>
 
 </html>
